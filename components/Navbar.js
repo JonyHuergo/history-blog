5ca247bfa6b351c8e64861b32e0bdaa799556886
Jonathan Huergo <jh2000@hotmail.com.ar>
2022-12-23 08:43:16 -0300

    Create Navbar component

---
+++ components/Navbar.js
@@ -0,0 +1,9 @@
+const Navbar = () => {
+    return (
+        <div className="container text-white bg-transparent hover:bg-white hover:text-black">
+            <h1>HISTORIA</h1>
+        </div>
+    );
+}
+ 
+export default Navbar;
