ffd6bcd444f18aae92b997d5a9da588c6ec24628
Jonathan Huergo <jh2000@hotmail.com.ar>
2022-12-24 04:38:32 -0300

    Add Link Author and Date to PostCard
    Added a link to the title of the post, linking to the
    post itself. Displayed the Author along with their
    photo, and the date the post was published.

--- components/PostCard.js
+++ components/PostCard.js
@@ -1,8 +1,20 @@
+import { useEffect, useState } from "react";
 import Link from "next/link";
-import moment from "moment/moment";
 
+const { DateTime } = require("luxon");
 
+/* useEffect((post) => {
+    setPostDate((post) => DateTime.fromISO(post.createdAt).toLocaleString(DateTime.DATETIME_MED))
+}, []); */
+
+/* setPostDate((post) => DateTime.fromISO(post.createdAt).toLocaleString(DateTime.DATETIME_MED))
+ */
 const PostCard = ({ post }) => {
+    const [postDate, setPostDate] = useState("");
+    useEffect(() => {
+        let dt = DateTime.fromISO(post.createdAt)
+        setPostDate(() => dt.toLocaleString(DateTime.DATETIME_MED))
+    }, []); 
 
     return (
         <article className="bg-white shadow-lg rounded-lg">
@@ -14,9 +26,31 @@ const PostCard = ({ post }) => {
                 />
             </div>
             <div>
-                <h2 className="text-center text-4xl text-bold">{post.title}</h2>
-                <p>{post.excerpt}</p>
+                <Link href={`./posts/${post.slug}`}>
+                    <h2 className="text-center text-4xl text-bold">{post.title}</h2>
+                </Link> 
             </div>
+            <div>
+                <div>
+                    <img
+                        src={post.author.photo.url}
+                        alt={post.author.name}
+                        height="30px"
+                        width="30px"
+                        className="align-middle rounded-full"
+                    />
+                    <p>{post.author.name}</p>
+                </div>
+                <div>
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 inline mr-2 text-pink-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
+                    </svg>
+                    <span className="align-middle">{postDate}</span>
+                </div>
+            </div>
+            <div>
+                <p>{post.excerpt}</p>
+            </div>    
         </article>
     );
 }
