c359957a211c8bfca4286b98445f52b9a07d53c1
Jonathan Huergo <jh2000@hotmail.com.ar>
2022-12-24 09:09:52 -0300

    Add functions to fetch recent and related posts

--- components/PostWidget.js
+++ components/PostWidget.js
@@ -1,4 +1,19 @@
-const PostWidget = () => {
+import { useState, useEffect } from "react";
+import { getRelatedPosts, getRecentPosts } from '../services'
+
+const PostWidget = ({ categories, slug }) => {
+    const [relatedPosts, setRelatedPosts] = useState([])
+
+    useEffect(() => {
+        if(slug) {
+            getRelatedPosts(categories, slug)
+                .then((result) => setRelatedPosts(result))
+        } else {
+            getRecentPosts()
+                .then((result) => setRelatedPosts(result))
+        }
+    }, [slug])
+
     return (
         <section>
             POST WIDGET
