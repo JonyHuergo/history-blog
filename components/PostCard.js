dff6eb53b99ba9efbdfd356a597e09edafc3bd8f
Jonathan Huergo <jh2000@hotmail.com.ar>
2022-12-25 02:51:25 -0300

    Style PostCard component
    Centered the text and added padding and margins. Changed the color
    of the author, date, and calendar svg image. Changes to the font
    boldness.

--- components/PostCard.js
+++ components/PostCard.js
@@ -11,19 +11,19 @@ const PostCard = ({ post }) => {
     }, []); 
 
     return (
-        <article className="bg-neutral-light shadow-lg rounded-lg">
-            <div className="relative overflow-hidden pb-80">
+        <article className="bg-neutral-light shadow-lg rounded-lg lg:p-8 pb-12 mb-8">
+            <div className="relative overflow-hidden pb-80 mb-6">
                 <img
                     src={post.featuredImage.url}
                     alt={post.title}
-                    className="absolute h-80 w-full object-cover rounded-top-lg lg:rounded-lg"
+                    className="absolute h-80 w-full object-cover shadow-lg rounded-t-lg lg:rounded-lg"
                 />
             </div>
-            <div>
+            <div className="mb-8">
                 <h2 className="text-center text-4xl text-bold">{post.title}</h2>
             </div>
-            <div>
-                <div>
+            <div className="block lg:flex text-center items-center justify-center mb-8 w-full">
+                <div className="flex items-center justify-center mb-4 lg:mb-0 w-full lg:w-auto mr-8">
                     <img
                         src={post.author.photo.url}
                         alt={post.author.name}
@@ -31,17 +31,17 @@ const PostCard = ({ post }) => {
                         width="30px"
                         className="align-middle rounded-full"
                     />
-                    <p>{post.author.name}</p>
+                    <p className="inline align-middle text-gray-700 ml-2 font-medium text-lg">{post.author.name}</p>
                 </div>
-                <div>
-                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 inline mr-2 text-pink-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <div className="font-medium text-gray-700">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 inline mr-2 text-secondary" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
                     </svg>
                     <span className="align-middle">{postDate}</span>
                 </div>
             </div>
             <div>
-                <p>{post.excerpt}</p>
+                <p className="text-center text-lg font-normal px-4 lg:px-20 mb-8">{post.excerpt}</p>
             </div>
             <div className="text-center">
                 <Link href={`./posts/${post.slug}`}>
