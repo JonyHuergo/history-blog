f6974f37743445d80567f0ba8b08190210146d6d
Jonathan Huergo <jh2000@hotmail.com.ar>
2022-12-28 01:34:50 -0300

    Add post content to PostDetail

--- components/PostDetail.js
+++ components/PostDetail.js
@@ -1,3 +1,4 @@
+import React from "react";
 import { useEffect, useState } from "react";
 
 const { DateTime } = require("luxon");
@@ -7,7 +8,46 @@ const PostDetail = ({ post }) => {
     useEffect(() => {
         let dt = DateTime.fromISO(post.createdAt)
         setPostDate(() => dt.toLocaleString(DateTime.DATETIME_MED))
-    }, []); 
+    }, []);
+
+    const getContentFragment = (index, text, obj, type) => {
+        let modifiedText = text;
+
+        if (obj) {
+            if (obj.bold) {
+                modifiedText = (<b key={index}>{text}</b>);
+            }
+
+            if (obj.italic) {
+                modifiedText = (<em key={index}>{text}</em>);
+            }
+
+            if (obj.underline) {
+                modifiedText = (<u key={index}>{text}</u>);
+            }
+        }
+
+        switch (type) {
+            case 'heading-three':
+                return <h3 key={index} className="text-xl font-semibold mb-4">{modifiedText.map((item, i) => <React.Fragment key={i}>{item}</React.Fragment>)}</h3>;
+            case 'paragraph':
+                return <p key={index} className="mb-8">{modifiedText.map((item, i) => <React.Fragment key={i}>{item}</React.Fragment>)}</p>;
+            case 'heading-four':
+                return <h4 key={index} className="text-md font-semibold mb-4">{modifiedText.map((item, i) => <React.Fragment key={i}>{item}</React.Fragment>)}</h4>;
+            case 'image':
+                return (
+                <img
+                    key={index}
+                    alt={obj.title}
+                    height={obj.height}
+                    width={obj.width}
+                    src={obj.src}
+                />
+                );
+            default:
+                return modifiedText;
+        }
+    };
 
     return (
         <article className="bg-neutral-light shadow-lg rounded-lg lg:p-8 pb-12 mb-8">
@@ -38,6 +78,11 @@ const PostDetail = ({ post }) => {
                 </div>
                 </div>
                 <h1 className="mb-8 text-3xl font-semibold">{post.title}</h1>
+                {post.content.raw.children.map((typeObj, index) => {
+                    const children = typeObj.children.map((item, itemIndex) => getContentFragment(itemIndex, item.text, item))
+
+                    return getContentFragment(index, children, typeObj, typeObj.type)
+                })}
             </div>
         </article>
     );
