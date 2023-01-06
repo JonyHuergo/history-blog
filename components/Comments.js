99df54348e125f9925ff6eb76d2097f8196cf5a7
Jonathan Huergo <jh2000@hotmail.com.ar>
2023-01-06 09:58:43 -0300

    Add the capability to display a post comments

--- components/Comments.js
+++ components/Comments.js
@@ -1,11 +1,32 @@
 import React, { useEffect, useState } from 'react';
-import parse from 'html-react-parser';
+import { getComments } from '../services';
+import Comment from './Comment';
 
-const Comments = () => {
+
+const Comments = ({slug}) => {
+    const [comments, setComments] = useState([]);
+
+    useEffect(() => {
+        getComments(slug).then((result) => {
+            setComments(result);
+        });
+    }, []);
+
     return (
-        <div>
-            Comments
-        </div>
+         <>
+        {comments.length > 0 && (
+            <div className="bg-white shadow-lg rounded-lg p-8 pb-12 mb-8">
+            <h3 className="text-xl mb-8 font-semibold border-b pb-4">
+                {comments.length}
+                {' '}
+                {comments.length > 1 ? "Comentarios" : "Comentario" }
+            </h3>
+                {comments.map((comment, index) => (
+                    <Comment index={index} comment={comment}/>
+                ))}
+            </div>
+        )}
+        </>
     );
 }
  
