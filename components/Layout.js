ccf82de4709970e218706f6c91f0dbc4fe87ac91
Jonathan Huergo <jh2000@hotmail.com.ar>
2023-01-10 08:50:30 -0300

    Create SearchBar component

--- components/Layout.js
+++ components/Layout.js
@@ -1,9 +1,11 @@
 import Navbar from './Navbar'
+import SearchBar from './SearchBar'
 
 const Layout = ({children}) => {
     return ( 
         <div className='lg:px-[5%]'>
             <Navbar/>
+            <SearchBar placeholder="Buscar posts"/>
             <main>
                 {children}
             </main>
---
+++ components/SearchBar.js
@@ -0,0 +1,13 @@
+const SearchBar = ({ placeholder, data }) => {
+    return (
+        <div>
+            <div>
+                <input type="text" placeholder={placeholder} />
+                <div></div>
+            </div>
+            <div></div>
+        </div>
+    );
+}
+ 
+export default SearchBar;
