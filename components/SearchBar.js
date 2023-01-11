fc24cbced2a14cfe3d3e333d8dec16064f80fea6
Jonathan Huergo <jh2000@hotmail.com.ar>
2023-01-11 01:54:00 -0300

    Create filter for search bar

--- components/SearchBar.js
+++ components/SearchBar.js
@@ -1,19 +1,36 @@
+import { useState, useEffect } from "react";
 import SearchIcon from '@mui/icons-material/Search';
 import Link from 'next/link';
 
 const SearchBar = ({ placeholder, data }) => {
+    const [filteredData, setFilteredData] = useState([]);
+
+    const handleFilter = (event) => {
+        const searchWord = event.target.value;
+        const newFilter = data.filter((value) => {
+            return value.node.title.toLowerCase().includes(searchWord.toLowerCase());
+        });
+
+        if (searchWord === "") {
+            setFilteredData([]);
+        } else {
+            setFilteredData(newFilter);
+        }
+    }
+
     return (
         <div>
             <div className="float-right mt-4 relative">
-                <input className="w-40 md:w-80 lg:w-[30rem] h-10 rounded-lg border-2 border-black hover:shadow-xl p-2" type="text" placeholder={placeholder} />
+                <input className="w-40 md:w-80 lg:w-[30rem] h-10 rounded-lg border-2 border-black hover:shadow-xl p-2" type="text" placeholder={placeholder} onChange={handleFilter} />
                 <div className='absolute right-2 top-2'>
                     <SearchIcon/>
                 </div>
+                {filteredData.length != 0 && (
                 <div
-                    className='bg-white rounded-lg absolute left-0 bottom--20 z-50 border-2 border-black mt-2 h-[10rem] overflow-scroll overflow-x-hidden
+                    className='bg-white rounded-lg absolute left-0 bottom--20 z-50 border-2 border-black mt-2 max-h-[10rem] overflow-scroll overflow-x-hidden
                     scrollbar-thin scrollbar-thumb-gray-400 scrollbar-track-transparent scrollbar-thumb-rounded-full scrollbar-track-rounded-full'
                 >
-                    {data.map((post, index) => {
+                    {filteredData.map((post, index) => {
                         return (
                             <div key={index} className="hover:bg-gray-300">
                             <Link href={`../post/${post.node.slug}`}>
@@ -25,6 +42,7 @@ const SearchBar = ({ placeholder, data }) => {
                         )
                     })}
                 </div>
+                )}
             </div>
         </div>
     );
