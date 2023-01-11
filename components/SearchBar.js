f424e76aa9de6f37a4365448e86fa1799814064e
Jonathan Huergo <jh2000@hotmail.com.ar>
2023-01-11 07:09:51 -0300

    Add X icon to close SearchBar results

--- components/SearchBar.js
+++ components/SearchBar.js
@@ -1,9 +1,11 @@
 import { useState, useEffect } from "react";
 import SearchIcon from '@mui/icons-material/Search';
+import CloseIcon from '@mui/icons-material/Close';
 import Link from 'next/link';
 
 const SearchBar = ({ placeholder, data }) => {
     const [filteredData, setFilteredData] = useState([]);
+    const [wordEntered, setwordEntered] = useState("");
 
     const removeAccents = (str) => {
         return str.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
@@ -11,6 +13,7 @@ const SearchBar = ({ placeholder, data }) => {
 
     const handleFilter = (event) => {
         const searchWord = removeAccents(event.target.value.toLowerCase());
+        setwordEntered(searchWord)
         const newFilter = data.filter((value) => {
             const title = removeAccents(value.node.title.toLowerCase())
             return title.includes(searchWord);
@@ -23,19 +26,33 @@ const SearchBar = ({ placeholder, data }) => {
         }
     }
 
+    const clearInput = () => {
+        setFilteredData([]);
+        setwordEntered("")
+    }
+
     return (
         <div>
             <div className="float-right mt-4 relative">
-                <input className="w-40 md:w-80 lg:w-[30rem] h-10 rounded-lg border-2 border-black hover:shadow-xl p-2" type="text" placeholder={placeholder} onChange={handleFilter} />
+                <input
+                    className="w-40 md:w-80 lg:w-[30rem] h-10 rounded-lg border-2 border-black hover:shadow-xl p-2"
+                    type="text"
+                    placeholder={placeholder}
+                    onChange={handleFilter}
+                    value={wordEntered}
+                />
                 <div className='absolute right-2 top-2'>
-                    <SearchIcon/>
+                    {wordEntered.length === 0 ?
+                        <SearchIcon/> :
+                        <CloseIcon className="cursor-pointer" onClick={clearInput} />
+                    }
                 </div>
                 {filteredData.length != 0 && (
                 <div
                     className='bg-white rounded-lg absolute left-0 bottom--20 z-50 border-2 border-black mt-2 max-h-[10rem] overflow-scroll overflow-x-hidden
                     scrollbar-thin scrollbar-thumb-gray-400 scrollbar-track-transparent scrollbar-thumb-rounded-full scrollbar-track-rounded-full'
                 >
-                    {filteredData.map((post, index) => {
+                    {filteredData.slice(0, 15).map((post, index) => {
                         return (
                             <div key={index} className="hover:bg-gray-300">
                             <Link href={`../post/${post.node.slug}`}>
